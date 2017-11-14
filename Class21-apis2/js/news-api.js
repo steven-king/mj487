99938e88ce2150034beccf85a9c07089c1ef3ac2
steven-king <steven.king@unc.edu>
2017-11-13 22:47:04 -0500

    fixing script call

--- Class21-apis2/js/news-api.js
+++ Class21-apis2/js/news-api.js
@@ -40,40 +40,6 @@ function parseSources(data){
 
 
 
-function loadArticles(source){
-
-    $.ajax({
-            type:"GET",
-            url:"https://newsapi.org/v1/articles?source=" + source + "&sortBy=latest&apiKey=b33a41de4be74829a057f2248c0a40dc",
-            dataType:"json",
-            success: parseArticles
-});
-
-
-}
-
-function parseArticles(data){
-  console.log(data);
-  var html = ""
-  var articles = [];
-  var tempPath = data["articles"];
-
-  for (var i = 0, len = tempPath.length; i < len; ++i) {
-        //console.log(i);
-         //sets data to arrays for charts
-         articles.push(tempPath[i]);
-        console.log(articles[0]["title"]);
-        html += '<li><a href="' + articles[0]["url"] + '">' + tempPath[i]["title"] + '</a></li>'
-
-  }
-  $("#feed-area").html(html);
-  console.log(html);
-
-
-}
-
-
-
 
 
 //https://newsapi.org/v1/articles?source=techcrunch&apiKey=b33a41de4be74829a057f2248c0a40dc
